ba40164ffb2701ffee35c93a0d802f136d58d7a0
Michael Schaffner <michael@oxygenbox.com>
2017-06-20 13:42:50 -0500

    Working on Main LAyout

---
+++ easyIntervals/RoundStatusView.swift
@@ -0,0 +1,19 @@
+//
+//  RoundStatusView.swift
+//  easyIntervals
+//
+//  Created by Michael Schaffner on 6/20/17.
+//  Copyright © 2017 Michael Schaffner. All rights reserved.
+//
+
+import UIKit
+
+class RoundStatusView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        layer.cornerRadius = frame.size.height/2
+        clipsToBounds = true
+    }
+    
+
+}
