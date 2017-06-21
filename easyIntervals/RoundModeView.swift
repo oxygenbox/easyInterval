657631487909f103dc753ecb8b325beeec878230
Michael Schaffner <michael@oxygenbox.com>
2017-06-21 12:52:53 -0500

    added constarints to RoundModeView

--- easyIntervals/RoundModeView.swift
+++ easyIntervals/RoundModeView.swift
@@ -14,19 +14,19 @@ class RoundModeView: UIView {
     let shrinkSize: CGFloat = 0.25
     let animationSpeed: Double = 0.25
     
-//    lazy var intervalClock: ClockView = {
-//        let clock = ClockView(frame: self.bounds)
-//        return clock
-//    }()
+    lazy var intervalClock: ClockView = {
+        let clock = ClockView(frame: self.bounds)
+        return clock
+    }()
     
-//    lazy var imageView: UIImageView = {
-//        let inset = self.frame.size.height * 0.10
-//        let rect = CGRect(x: inset, y: inset, width: self.bounds.width - inset*2, height: self.bounds.height - inset*2)
-//        let iv = UIImageView(frame: rect)
-//        iv.contentMode = .scaleAspectFit
-//        iv.tintColor = UIColor.white
-//        return iv
-//    }()
+    lazy var imageView: UIImageView = {
+        let inset = self.frame.size.height * 0.10
+        let rect = CGRect(x: inset, y: inset, width: self.bounds.width - inset*2, height: self.bounds.height - inset*2)
+        let iv = UIImageView(frame: rect)
+        iv.contentMode = .scaleAspectFit
+        iv.tintColor = UIColor.white
+        return iv
+    }()
     
     var mode: Mode = .run {
         didSet {
@@ -42,9 +42,6 @@ class RoundModeView: UIView {
         }
     }
     
-    @IBOutlet weak var intervalClock: ClockView!
-    @IBOutlet weak var imageView: UIImageView!
-    
     
     override func draw(_ rect: CGRect) {
         layer.cornerRadius = frame.size.height/2
@@ -52,8 +49,20 @@ class RoundModeView: UIView {
     }
     
     func configure() {
-       // addSubview(intervalClock)
-       // addSubview(imageView)
+        addSubview(intervalClock)
+        addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerXAnchor.constraint(equalTo: imageView.superview!.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: imageView.superview!.centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: frame.size.height * 0.8).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: frame.size.width * 0.8).isActive = true
+        
+        intervalClock.translatesAutoresizingMaskIntoConstraints = false
+        intervalClock.centerXAnchor.constraint(equalTo: intervalClock.superview!.centerXAnchor).isActive = true
+        intervalClock.centerYAnchor.constraint(equalTo: intervalClock.superview!.centerYAnchor).isActive = true
+        intervalClock.heightAnchor.constraint(equalToConstant: frame.size.height).isActive = true
+        intervalClock.widthAnchor.constraint(equalToConstant: frame.size.width).isActive = true
         
         switch mode {
         case .run:
@@ -70,7 +79,7 @@ class RoundModeView: UIView {
     
     func beginClock(intervalSeconds: Int) {
         intervalClock.shapeLayer.strokeColor = self.clockColor.cgColor
-        intervalClock.shapeLayer.lineWidth = frame.size.width - 12
+        intervalClock.shapeLayer.lineWidth = intervalClock.frame.size.width * 0.92 //- 12
         intervalClock.begin(with: intervalSeconds)
     }
     
