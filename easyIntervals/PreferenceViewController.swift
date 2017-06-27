d8bdf95bad7320bc38dc069d4434235cf3519675
Michael Schaffner <michael@oxygenbox.com>
2017-06-26 20:48:58 -0500

    replaced missing images

--- easyIntervals/PreferenceViewController.swift
+++ easyIntervals/PreferenceViewController.swift
@@ -203,8 +203,10 @@ class PreferenceViewController: UIViewController {
     func initSegmentedControls() {
         intervalOrderControl.tintColor = UIColor.activeButton
         intervalOrderControl.backgroundColor = UIColor.white.withAlphaComponent(0.25)
-        sessionControl.tintColor = UIColor.packDark
-        cadenceControl.tintColor = UIColor.packDark
+        sessionControl.tintColor = UIColor.activeButton
+        cadenceControl.tintColor = UIColor.activeButton
+        sessionControl.backgroundColor = UIColor.white.withAlphaComponent(0.2)
+        cadenceControl.backgroundColor = UIColor.white.withAlphaComponent(0.2)
         
         sessionControl.isHidden = activePreference != .workout
         cadenceControl.isHidden = activePreference != .cadence
